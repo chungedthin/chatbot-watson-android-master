e3347fcbf420594bd0f091244ca985436f900e33
Edthin <chungedthin@gmail.com>
2020-05-08 09:31:03 +0800

    new version

--- app/src/main/java/com/example/vmac/WatBot/MainActivity.java
+++ app/src/main/java/com/example/vmac/WatBot/MainActivity.java
@@ -1,14 +1,21 @@
 package com.example.vmac.WatBot;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Typeface;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.net.Uri;
 import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.speech.RecognitionListener;
+import android.speech.RecognizerIntent;
+import android.speech.SpeechRecognizer;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
@@ -19,6 +26,7 @@ import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
@@ -172,20 +180,118 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        btnRecord.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-//                recordMessage();
-                Intent intent = new Intent(MainActivity.this, com.example.vmac.WatBot.SpeechToText.class);
-                startActivity(intent);
-            }
-        });
+        speechToText();
+
+//        btnRecord.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+////                recordMessage();
+//                Intent intent = new Intent(MainActivity.this, com.example.vmac.WatBot.SpeechToText.class);
+//                startActivity(intent);
+//            }
+//        });
 //
 //        createServices();
 //        sendMessage();
     };
 
 
+
+  @SuppressLint("ClickableViewAccessibility")
+  public void speechToText(){
+      checkPermission();
+      final SpeechRecognizer mSpeechRecognizer = SpeechRecognizer.createSpeechRecognizer(this);
+      final Intent mSpeechRecognizerIntent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+      mSpeechRecognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+      mSpeechRecognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, "zh-HK");
+      mSpeechRecognizer.setRecognitionListener(new RecognitionListener() {
+          @Override
+          public void onReadyForSpeech(Bundle bundle) {
+
+          }
+
+          @Override
+          public void onBeginningOfSpeech() {
+
+          }
+
+          @Override
+          public void onRmsChanged(float v) {
+
+          }
+
+          @Override
+          public void onBufferReceived(byte[] bytes) {
+
+          }
+
+          @Override
+          public void onEndOfSpeech() {
+
+          }
+
+          @Override
+          public void onError(int i) {
+
+          }
+
+          @Override
+          public void onResults(Bundle bundle) {
+              //getting all the matches
+              ArrayList<String> matches = bundle
+                      .getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
+
+              //displaying the first match
+              if (matches != null)
+                  inputMessage.setText(matches.get(0));
+          }
+
+          @Override
+          public void onPartialResults(Bundle bundle) {
+
+          }
+
+          @Override
+          public void onEvent(int i, Bundle bundle) {
+
+          }
+      });
+
+
+
+      btnRecord.setOnTouchListener(new View.OnTouchListener() {
+          @Override
+          public boolean onTouch(View view, MotionEvent motionEvent) {
+              switch (motionEvent.getAction()) {
+                  case MotionEvent.ACTION_UP:
+                      mSpeechRecognizer.stopListening();
+                      inputMessage.setHint("You will see input here");
+                      break;
+
+                  case MotionEvent.ACTION_DOWN:
+                      mSpeechRecognizer.startListening(mSpeechRecognizerIntent);
+                      inputMessage.setText("");
+                      inputMessage.setHint("Listening...");
+                      break;
+              }
+              return false;
+          }
+      });
+
+  }
+
+
+
+    private void checkPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (!(ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO) == PackageManager.PERMISSION_GRANTED)) {
+                Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS,
+                        Uri.parse("package:" + getPackageName()));
+                startActivity(intent);
+                finish();
+            }
+        }
+    }
 
 
     @Override
